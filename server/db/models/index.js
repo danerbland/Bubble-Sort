5dc0fc32afb59b25a487f3e1bf4fece05eefbb9b
Dan Erbland <danerbland@gmail.com>
2020-02-20 16:48:20 -0500

    initialized models index.js

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,5 @@
 const Order = require('./order')
+const OrderProduct = require('./orderProduct')
 const Product = require('./product')
 const Review = require('./review')
 const User = require('./user')
@@ -6,4 +7,17 @@ const User = require('./user')
 Order.belongsTo(User)
 User.hasMany(Order)
 
+User.belongsToMany(Product, {through: Review})
+Product.belongsToMany(User, {through: Review})
 
+Product.belongsToMany(Order, {through: OrderProduct})
+Order.belongsToMany(Product, {through: OrderProduct})
+
+
+module.exports = {
+  Order,
+  OrderProduct,
+  Product,
+  Review,
+  User
+}
